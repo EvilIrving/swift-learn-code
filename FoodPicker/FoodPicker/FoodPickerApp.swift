bbf0bf8d78af2ac452d0b4a0cc1b04165393353f
dongtianbao <dong.yy1916@gmail.com>
2023-10-19 00:24:52 +0800

    feat: 新增吃什么

---
+++ FoodPicker/FoodPicker/FoodPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPickerApp.swift
+//  FoodPicker
+//
+//  Created by Actor on 2023/10/18.
+//
+
+import SwiftUI
+
+@main
+struct FoodPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

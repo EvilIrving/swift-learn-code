f3047a0d523c88e3fb84330847917500bbaba684
evilirving <dong.yy1916@gmail.com>
2024-01-28 14:12:13 +0800

    feat: 第一节

--- food_picker/food_picker/food_pickerApp.swift
+++ food_picker/food_picker/food_pickerApp.swift
@@ -7,6 +7,16 @@
 
 import SwiftUI
 
+// 代表 app 的进入点 必须 conform App,body 属性中设计 Scene的类型
+// App -> Scene -> View
+/*
+ ContentView & 预览画面
+ 专案和 target 设定
+ @main & App & Scene
+ assets & preview content
+ iOS 最低版本选择
+ */
+
 @main
 struct food_pickerApp: App {
     var body: some Scene {
